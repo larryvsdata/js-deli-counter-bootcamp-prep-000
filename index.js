790a9000096ba2a32895ba07eb1558b87cda68ec
larryvsdata <ermanbekaroglu@gmail.com>
2018-09-05 17:47:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function nowServing(katzDeliLine){
     console.log("There is nobody waiting to be served!");
   } else
   {
-    console.log("Currently serving "+katzDeliLine.shift()+".");
+    console.log("Currently serving customer number "+katzDeliLine.shift()+".");
   }
   
 }
